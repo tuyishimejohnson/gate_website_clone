2a0673a126b67d7bdd0bd97a3c7fe7bee3242c0a
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-05-17 11:08:09 +0200

    Added position absolute

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-image': "url('images/backgroundImage.jpeg')",
+    },
   },
   plugins: [],
 }
-
+}
